b16020cb2b37d03fa69a1e628d5b6b34a7a547a3
gaugfather <derek.gauger@gmail.com>
2021-01-30 15:58:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# arduino-breathalyzer
+
+## My first arduino project.  
+
+Props to the tutorial on https://www.electronics-lab.com/project/arduino-breathalyzer-using-mq3-gas-sensor-oled-display/
---
+++ breathalyzer.ino
@@ -0,0 +1,133 @@
+//Copied by The Gaugfather
+
+
+// thanks to https://www.electronics-lab.com/project/arduino-breathalyzer-using-mq3-gas-sensor-oled-display/
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define OLED_RESET 4
+int TIME_UNTIL_WARMUP = 5; //900;
+unsigned long time;
+
+
+int analogPin = 0;
+int val = 0;
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+void setup()   {                
+
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.clearDisplay();
+}
+
+
+void loop() {  
+  
+  delay(100);
+
+  val = readAlcohol();
+  printTitle();
+  printWarming();
+
+  time = millis()/1000;
+  
+  if(time<=TIME_UNTIL_WARMUP)
+  {
+    time = map(time, 0, TIME_UNTIL_WARMUP, 0, 100);
+    display.drawRect(10, 50, 110, 10, WHITE); //Empty Bar
+    display.fillRect(10, 50, time,10,WHITE);
+  }else
+  {
+     printTitle();
+     printAlcohol(val);
+     printAlcoholLevel(val);  
+  }
+  display.display();
+
+}
+
+
+void printTitle()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(BLACK,WHITE);
+  display.setCursor(10,0);
+  display.println(" Gaug Breathalyzer ");
+}
+
+void printWarming()
+{
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("Warming up");
+}
+
+void printAlcohol(int value)
+{
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,12);
+  display.println(val);
+}
+
+void printAlcoholLevel(int value)
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(42,15);
+
+  String step1 = " 0/3 -  Sober!";
+  //String step2 = " +1 Cheers!";
+  String step3 = " 1/3 - Cheers!";
+  String step4 = " 2/3 - Naked!";
+  String step5 = " 3/3 - Puking!";
+  
+  if(value<210)
+  {
+      display.println(step1);
+  }
+  //if (value>=200 && value<280)
+  //{
+  //    display.println(step2);
+ // }
+  //if (value>=280 && value<350)
+  if (value >=215 && value <350)
+  {
+      display.println(step3);
+  }
+  if (value>=350 && value <450)
+  {
+      display.println(step4);
+  }
+  if(value>450)
+  {
+     display.println(step5);
+  }
+ }
+ 
+ int readAlcohol()
+ {
+  int val = 0;
+  int val1;
+  int val2;
+  int val3; 
+
+
+  display.clearDisplay();
+  val1 = analogRead(analogPin); 
+  delay(10);
+  val2 = analogRead(analogPin); 
+  delay(10);
+  val3 = analogRead(analogPin);
+  
+  val = (val1+val2+val3)/3;
+  return val;
+ }
